ea8c8d3c60739f90d8bdeb739b8b104d0d1eb3d9
Pawel <paweldolak2003@gmail.com>
2024-06-12 18:26:17 +0200

    preszkody i poprawki

--- Endlesrunner/Endlesrunner.cpp
+++ Endlesrunner/Endlesrunner.cpp
@@ -10,6 +10,7 @@ struct Obstacle {
     float width;
     float height;
     float length;
+    int type;
 };
 
 int main() {
@@ -17,7 +18,6 @@ int main() {
     const int screenHeight = 800;
 
     InitWindow(screenWidth, screenHeight, "Endless Runner");
-
     // Definicja kamery
     Camera camera = { 0 };
     camera.position = Vector3{ 0.0f, 6.0f, 12.0f };
@@ -30,30 +30,32 @@ int main() {
     Vector3 playerPosition = { 0.0f, 1.0f, 2.0f };
     float playerWidth = 1.0f;
     float playerHeight = 2.0f;
-    float playerSpeed = 15.0f;
+    float playerSpeed = 25.0f;
     float gravity = 20.0f;
-    float jumpSpeed = 8.0f;
+    float jumpSpeed = 10.0f;
     bool isJumping = false;
     float verticalSpeed = 0.0f;
-    
+
     int score = 0;
+    bool gameOver = false;
 
     // Inicjalizacja przeszkód
+    Model laser = LoadModel("laser.glb");
+    Model beczka = LoadModel("barrel.glb");
     std::vector<Obstacle> obstacles;
     bool isObstacle = false;
     float obstacleTimer = 0.0f;
+    float obstacleTimer_zero = 2.0f;
     srand(time(0));
 
-    //Dodawanie drogi
+    // Dodawanie drogi
     Texture2D texture = LoadTexture("cubicmap_atlas.png");
     Mesh cubeMesh = GenMeshCube(1.0f, 1.0f, 1.0f);
     Model cubeModel = LoadModelFromMesh(cubeMesh);
     cubeModel.materials[0].maps[MATERIAL_MAP_ALBEDO].texture = texture;
     std::vector<Obstacle> droga;
-    for (int i = 7; i >= -100; i--)
-    {
-        for (int j = -4; j <= 4; j++)
-        {
+    for (int i = 7; i >= -100; i--) {
+        for (int j = -4; j <= 4; j++) {
             Obstacle path;
             path.position = Vector3{ float(j), 0.2f, float(i) };
             path.width = 1.0f;
@@ -63,9 +65,7 @@ int main() {
         }
     }
 
-
-
-    //Dodawanie robota
+    // Dodawanie robota
     Model robot = LoadModel("robot.glb");
     int animsCount = 7;
     unsigned int animIndex = 6;
@@ -73,127 +73,139 @@ int main() {
     ModelAnimation* modelAnimations = LoadModelAnimations("robot.glb", &animsCount);
 
     SetTargetFPS(60);
-    
 
     while (!WindowShouldClose()) {
-
-        //Generowanie przeszkod
-        obstacleTimer += GetFrameTime();
-        if (obstacleTimer > 2.0f)
-        {
-            isObstacle = true;
-            obstacleTimer = 0;
-        }
-        if (isObstacle)
-        {
-            Obstacle obs;
-            obs.position = Vector3{ float(rand() % 3 * 3 - 3), 1.0f, -100.0f };
-            obs.width = 2.0f;
-            obs.height = 2.0f;
-            obs.length = 2.0f;
-            obstacles.push_back(obs);
-            isObstacle = false;
-        }
-
-        // Aktualizacja gracza
-        if (playerPosition.x > -3)
-        {
-            if (IsKeyPressed(KEY_LEFT)) playerPosition.x -= 3;
-        }
-        if (playerPosition.x < 3)
-        {
-            if (IsKeyPressed(KEY_RIGHT)) playerPosition.x += 3;
-        }
-        if (IsKeyPressed(KEY_SPACE) && !isJumping) {
-            isJumping = true;
-            verticalSpeed = jumpSpeed;
-        }
-        if (isJumping) {
-            verticalSpeed -= gravity * GetFrameTime();
-            playerPosition.y += verticalSpeed * GetFrameTime();
-
-            if (playerPosition.y <= 1.0f) {
-                playerPosition.y = 1.0f;
-                isJumping = false;
-                verticalSpeed = 0.0f;
+        if (!gameOver) {
+            // Generowanie przeszkod
+            obstacleTimer += GetFrameTime();
+            if (obstacleTimer > obstacleTimer_zero) {
+                isObstacle = true;
+                obstacleTimer = 0;
             }
-        }
+            if (isObstacle) {
+                Obstacle obs;
+                obs.position = Vector3{ float(rand() % 3 * 3 - 3), 1.0f, -100.0f };
+                obs.width = 2.0f;
+                obs.height = 1.0f;
+                obs.length = 2.0f;
+                obs.type = rand() % 2;
+                obstacles.push_back(obs);
+                isObstacle = false;
+            }
 
-        // Animacja robota
-        ModelAnimation anim = modelAnimations[animIndex];
-        animCurrentFrame = (animCurrentFrame + 2) % anim.frameCount;
-        UpdateModelAnimation(robot, anim, animCurrentFrame);
+            // Aktualizacja gracza
+            if (playerPosition.x > -3) {
+                if (IsKeyPressed(KEY_LEFT)) playerPosition.x -= 3;
+            }
+            if (playerPosition.x < 3) {
+                if (IsKeyPressed(KEY_RIGHT)) playerPosition.x += 3;
+            }
+            if (IsKeyPressed(KEY_SPACE) && !isJumping) {
+                isJumping = true;
+                verticalSpeed = jumpSpeed;
+                animCurrentFrame = 0;
+            }
+            if (isJumping) {
+                verticalSpeed -= gravity * GetFrameTime();
+                playerPosition.y += verticalSpeed * GetFrameTime();
+                animIndex = 3;
+                
 
-        //aktualizacja wyniku
-        score++;
+                if (playerPosition.y <= 1.0f) {
+                    playerPosition.y = 1.0f;
+                    isJumping = false;
+                    verticalSpeed = 0.0f;
+                    animIndex = 6;
+                }
+            }
 
-        //restart
-        if (IsKeyPressed(KEY_R))
-        {
-            obstacles.clear();
-            DisableEventWaiting();
-            score = 0;
-        }
-
-        
-
-        BeginDrawing();
-
-        // Aktualizacja przeszkód
-        for (auto& obs : obstacles) {
-            obs.position.z += playerSpeed * GetFrameTime();
-
-            int pointzero = obs.position.z;
-            if (playerPosition.z == pointzero && playerPosition.x == obs.position.x)
+            // Animacja robota
+            ModelAnimation anim = modelAnimations[animIndex];
+            if(animIndex==3)
             {
-                DrawText("GAME OVER", 100, 100, 50, BLACK);
-                EnableEventWaiting();
+                if (animCurrentFrame <= anim.frameCount) animCurrentFrame++;
+
             }
-            if (obs.position.z > camera.position.z + 10.0f) {
-                obstacles.erase(obstacles.begin());
+            else  animCurrentFrame = (animCurrentFrame + 2) % anim.frameCount;
+            UpdateModelAnimation(robot, anim, animCurrentFrame);
+
+            // aktualizacja wyniku i predkosci
+            score++;
+            if (score % 500 == 0)
+            {
+                playerSpeed += 5.0f;
+                obstacleTimer_zero *= 0.8f;
+            }
+
+
+            // Aktualizacja przeszkód
+            for (auto& obs : obstacles) {
+                obs.position.z += playerSpeed * GetFrameTime();
+
+                int pointzero = obs.position.z;
+                
+                if (playerPosition.z == pointzero && playerPosition.x == obs.position.x && playerPosition.y == obs.position.y)  gameOver = true;
+                
+                if (obs.position.z > camera.position.z + 10.0f) obstacles.erase(obstacles.begin());           
             }
         }
+        // restart
+        if (IsKeyPressed(KEY_R)) {
+            obstacles.clear();
+            score = 0;
+            playerSpeed = 25.0f;
+            obstacleTimer_zero = 2.0f;
+            gameOver = false;
+        }
 
-        //aktualizacja drogi //COS TU SIE SYPIE I WYWALA PROGRAM, ZEBY DZIALALO TRZEBA ZAKOMENTOWAC
-        //for (auto& path : droga) {
-        //    path.position.z += playerSpeed * GetFrameTime()/1.3;
+        // aktualizacja drogi // COS TU SIE SYPIE I WYWALA PROGRAM, ZEBY DZIALALO TRZEBA ZAKOMENTOWAC
+        // for (auto& path : droga) {
+        //     path.position.z += playerSpeed * GetFrameTime()/1.3;
 
-        //    if (path.position.z > camera.position.z + 10.0f) {
-        //        droga.push_back(path);
-        //        //droga.erase(droga.begin());
-        //    }
-        //}
-
-
+        //     if (path.position.z > camera.position.z + 10.0f) {
+        //         droga.push_back(path);
+        //         // droga.erase(droga.begin());
+        //     }
+        // }
+        BeginDrawing();
         ClearBackground(RAYWHITE);
 
         BeginMode3D(camera);
 
+        
+        // Rysowanie przeszkód
+        for (const auto& obs : obstacles) {
+            switch (obs.type) {
+            case 0: {
+                DrawModelEx(beczka, Vector3{ obs.position.x,  obs.position.y + 0.5f, obs.position.z, }, Vector3{ 0.0f, 1.0f, 0.0f }, 90.0f, Vector3{ 0.15f, 0.15f ,0.15f }, WHITE);
+                break;
+            }
+            case 1: {
+                DrawModelEx(laser, Vector3{ obs.position.x,  obs.position.y + 0.5f, obs.position.z, }, Vector3{ 1.0f, 0.0f, 0.0f }, 90.0f, Vector3{ 1.0f, 1.0f ,1.0f }, WHITE);
+                break;
+            }
+            }
+        }
+
         // Rysowanie gracza
         DrawModelEx(robot, playerPosition, Vector3{ 0.0f, 1.0f, 0.0f }, 180.0f, Vector3{ 0.3f, 0.3f, 0.3f }, WHITE);
 
-        // Rysowanie przeszkód
-        for (const auto& obs : obstacles) 
-        {
-            DrawCube(obs.position, obs.width, obs.height, obs.length, RED);
-            DrawCubeWires(obs.position, obs.width, obs.height, obs.length, BLACK);
-        }
-
-        //Rysowanie drogi
-        for (const auto& path : droga) 
-        {
+        // Rysowanie drogi
+        for (const auto& path : droga) {
             DrawModel(cubeModel, path.position, 1.0f, WHITE);
         }
-      /*  DrawModel(cubeModel, Vector3{ 0.0f, 0.15f, 1.0f }, 1.0f, WHITE);
-        DrawModel(cubeModel, Vector3{ -3.0f, 0.2f, 1.0f }, 1.0f, WHITE);
-        DrawModel(cubeModel, Vector3{ 3.0f, 0.1f, 1.0f }, 1.0f, WHITE);*/
-
-        //Pokazywanie FPS mi nie działa :(
-        DrawFPS(20, 20);
 
         EndMode3D();
 
-        DrawText("Ue LEFT and RIGHT to move, SPACE to jump", 10, 10, 20, DARKGRAY);
+        // Pokazywanie FPS
+        DrawFPS(20, 50);
+
+        // Game Over
+        if (gameOver)
+        {
+            DrawText("GAME OVER", 200, 60, 50, BLACK);
+        }
+        DrawText("Use LEFT and RIGHT to move, SPACE to jump", 10, 10, 20, DARKGRAY);
         DrawText(TextFormat("Score : %i", score), 10, 30, 20, DARKGRAY);
 
         EndDrawing();
